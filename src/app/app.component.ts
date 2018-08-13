06257c10f5624c17459ce8af5601aa26c5adb574
Ddakoshik <roobotik@gmail.com>
2018-08-13 08:53:57 +0300

    fix unic id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,28 @@ import { AppState } from './shared/redux/app.state';
 export class AppComponent implements OnInit {
   title = 'angular6-ngrx';
   public vihicles: Vihicle[] = [];
+  lastId: number;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
     this.store.select('carPage').subscribe(({cars}) => {
       this.vihicles = cars;
+      this.lastId = this.vihicles.length;
     });
+    console.log('oninit', this.lastId);
   }
   onAdd(car: Vihicle) {
-    car.id = this.vihicles.length + 1;
+    car.id = ++this.lastId;
+    console.log(this.lastId);
     this.vihicles.push(car);
   }
 
   onDelete(car: Vihicle) {
     this.vihicles = this.vihicles.filter(c => c.id !== car.id);
+  }
+
+  onEdit(car: Vihicle) {
+
   }
 }
--- src/app/shared/redux/cars.reducer.ts
+++ src/app/shared/redux/cars.reducer.ts
@@ -4,9 +4,9 @@ import { CAR_ACTION, AddCar } from './cars.action';
 
 const initialState = {
     cars: [
-        new Vihicle('Ford', 2014, 'code: e936d422-dd23-4576-97cc-35b1ae6b2209', 300, 1),
-        new Vihicle('Audi', 2002, 'code: e936d422-dd23-4576-97cc-35b1ae6b2209', 200, 2),
-        new Vihicle('Fiat', 2010, 'code: e936d422-dd23-4576-97cc-35b1ae6b2209', 150, 3)
+        new Vihicle('Ford', 2014, 'code: e936d422-dd23-4576-97cc-35b1ae6b2209', 300, 0),
+        new Vihicle('Audi', 2002, 'code: e936d422-dd23-4576-97cc-35b1ae6b2209', 200, 1),
+        new Vihicle('Fiat', 2010, 'code: e936d422-dd23-4576-97cc-35b1ae6b2209', 150, 2)
     ]
 };
 
